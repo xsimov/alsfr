e3813b268e35c1ca4f6940cda0199b44c4aaee42
Xavier Simó <xsimov@gmail.com>
2014-10-29 17:22:01 +0100

    Fixed bug for scores, it wasn't belongs_to but has_one because the key was in another model

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,5 +1,5 @@
 class Score < ActiveRecord::Base
   belongs_to :match
   belongs_to :player
-  belongs_to :game, through: :match
+  has_one :game, through: :match
 end
--- spec/factories/games_factory.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :game do
-    title "Juego de Tronos"
-    association :tag
-  end
-end
