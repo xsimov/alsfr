81a3d41f1d867fcbb22d9f0e6c7c965090277c87
Xavier Simó <xsimov@gmail.com>
2014-10-30 12:57:34 +0100

    implemented form for adding new games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,16 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.create game_params
+    flash[:notice] = "Joc afegit!"
+    redirect_to games_path
+  rescue
+    render :new
+  end
 end
