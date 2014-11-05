b3fa7375e58f49a75a6149018abba898a7d0ff18
Xavier Simó <xsimov@gmail.com>
2014-11-05 12:59:31 +0100

    added validation to prevent duration being nil or 0 when a match is closed

--- app/models/match.rb
+++ app/models/match.rb
@@ -6,7 +6,7 @@ class Match < ActiveRecord::Base
   scope :closed, -> { where(open_status: false).ordered }
   scope :ordered, -> { order(open_status: :desc).order(created_at: :desc) }
   accepts_nested_attributes_for :scores
-  validates :duration, presence: true, numericality: {greater_than: 0}, on: :set_scores
+  validates :duration, presence: true, numericality: {greater_than: 0}, on: :update, id: "closed?"
 
   def beautified_created_at
     self.created_at.strftime("%-d.%-m.%Y a les %l:%Mh %P")
@@ -15,5 +15,9 @@ class Match < ActiveRecord::Base
   def reset_scores_and_duration
     self.duration = 0
     self.scores.map { |score| score.points = 0;score.save }
+  end
+
+  def closed?
+    !self.open_status
   end
 end
